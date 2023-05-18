d6f9853d490cdd6d586c6bb614c2f44a54f4124a
chojangg <s2115@e-mirim.hs.kr>
2023-05-18 13:17:33 +0900

    간단한 급훈 출력 및 안내 기능 구현

--- WinFormPrac2/Form1.cs
+++ WinFormPrac2/Form1.cs
@@ -16,5 +16,10 @@ namespace WinFormPrac2
         {
             InitializeComponent();
         }
+
+        private void lblFormTitle_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("안녕하세요~");
+        }
     }
 }
